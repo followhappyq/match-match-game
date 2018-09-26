6c593b16f8188d530a0d3b3c5649fbfb792149c4
followhappyq <followhappyq@gmail.com>
2018-09-26 19:57:15 +0300

    code refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -2,33 +2,6 @@ const game = document.getElementById("new-game");
 const field = document.getElementsByClassName("game-field")[0];
 let cards;
 
-const cardsInfo = {
-  urlToFolder: "../images/",
-  shirt: "shirt.png",
-  info: [
-    {
-      id: 1,
-      name: "Alice",
-      url: "alice.png"
-    },
-    {
-      id: 2,
-      name: "Amon Ra",
-      url: "amonra.png"
-    },
-    {
-      id: 3,
-      name: "Baphomet",
-      url: "baphomet.png"
-    },
-    {
-      id: 4,
-      name: "Wild Cat",
-      url: "wild.png"
-    }
-  ]
-};
-
 const gameInfo = {
   playingField: [1, 3, 2, 4, 1, 2, 3, 4],
   cardClicked: "",
@@ -38,24 +11,19 @@ const gameInfo = {
 
 const startGame = e => {
   clearField();
-  shufflePlayindField();
+  shuffleCardPack();
   addCardsToField();
   addStopButton();
 };
 
-const stopGame = e => {
-  // if (!gameInfo.gameBegin) {
-  //   gameInfo.gameBegin = true;
-  //   field.innerHTML = "";
-  // }
-};
+const stopGame = e => {};
 
 const clearField = () => {
   field.innerHTML = "";
 };
 
-const shufflePlayindField = () => {
-  for (let i = 4; i >= 0; i--) {
+const shuffleCardPack = () => {
+  for (let i = 7; i >= 0; i--) {
     let randomIndex = Math.floor(Math.random() * (i + 1));
     let itemAtIndex = gameInfo.playingField[randomIndex];
     gameInfo.playingField[randomIndex] = gameInfo.playingField[i];
@@ -64,14 +32,12 @@ const shufflePlayindField = () => {
 };
 
 const addCardsToField = () => {
-  const { urlToFolder, info } = cardsInfo;
   const { playingField } = gameInfo;
+
   for (let i = 0; i < playingField.length; i++) {
     field.innerHTML += `
     <div class="flip-container">
-        <div class="game-element flipper" id="card_${
-          info[playingField[i] - 1].id
-        }">
+        <div class="game-element flipper" id="card_${playingField[i]}">
           <div class="front default" >
           </div><div class="back default">
         </div>
@@ -79,29 +45,20 @@ const addCardsToField = () => {
     </div>`;
   }
   cards = document.getElementsByClassName("game-element");
-
   for (let i = 0; i < 8; i++) {
     cards[i].addEventListener("click", clickOnTheCard);
+    console.log(cards[i]);
   }
 };
 
 const addStopButton = () => {};
 
 const clickOnTheCard = e => {
-  cardCheck(e);
-};
-
-const cardCheck = e => {
   const { id, childNodes } = e.target.parentNode;
 
   if (e.target.parentNode.className != "game-element flipper flipped") {
     e.target.parentNode.className = "game-element flipper flipped";
     childNodes[2].className = `back default opened-${id.slice(-1)}`;
-    if (isCardClicked(id)) {
-      e.target.parentNode.className = "game-element flipper flipped disabled";
-      cardDisabled(id);
-    } else {
-    }
   } else {
     e.target.parentNode.className = "game-element flipper";
     gameInfo.cardClicked = "";
@@ -113,10 +70,10 @@ const cardDisabled = id => {
 };
 
 const isCardClicked = id => {
-  if (id.slice(-1) == gameInfo.cardClicked) {
+  if (id == gameInfo.cardClicked) {
     return true;
   }
-  gameInfo.cardClicked = id.slice(-1);
+  gameInfo.cardClicked = id;
   return false;
 };
 
